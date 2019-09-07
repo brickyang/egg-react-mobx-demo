3975990eb803422410c16accf5a1239b82300d5d
Brick <brick.c.yang@gmail.com>
2019-09-07 22:23:41 +0800

    refactor: remove outdated comments

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -5,8 +5,6 @@ export default class HomeController extends Controller {
     const initialStore = this.service.store.initialStore;
 
     await this.ctx.render('index', {
-      // this.config is available, but it's type is BaseContextClass.config
-      // which can't access IConfig interface we need
       title: this.app.config.title,
       assets: require(this.app.config.assets),
       initialStore: JSON.stringify(initialStore),
